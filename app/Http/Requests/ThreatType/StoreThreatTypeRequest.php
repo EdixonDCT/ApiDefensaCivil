c0d843fb8092111d766ac01735b40516ecfe5464
EdixonDCT <edixondelta2022@gmail.com>
2026-02-09 11:49:13 -0500

    factor de peligro

--- app/Http/Requests/ThreatType/StoreThreatTypeRequest.php
+++ app/Http/Requests/ThreatType/StoreThreatTypeRequest.php
@@ -14,7 +14,7 @@ class StoreThreatTypeRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => 'required|string|max:255',
+            'name' => 'required|string|max:255|unique:threat_types,name',
             'is_active' => 'required|boolean',
         ];
     }
@@ -25,6 +25,7 @@ class StoreThreatTypeRequest extends FormRequest
             'name.required' => 'El nombre del tipo de amenaza es obligatorio.',
             'name.string' => 'El nombre debe ser un texto válido.',
             'name.max' => 'El nombre no puede superar los 255 caracteres.',
+            'name.unique' => 'El nombre del tipo de amenaza ya existe.',
 
             'is_active.required' => 'El estado es obligatorio.',
             'is_active.boolean' => 'El estado debe ser verdadero o falso.',
