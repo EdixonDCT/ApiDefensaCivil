7f535e04abd05bf8da9f3ed125bd50ea186ba35f
Breyiner <breyneracosta7@gmail.com>
2026-03-04 14:53:29 -0500

    modificar seeder de seccionales de la Defensa Civil Colombiana
    - 26 Direcciones Seccionales oficiales de la DCC
    - 4 Oficinas regionales (Amazonas, Arauca, Putumayo, San Andrés)
    - Cada seccional incluye registro de auditoría simulado por Sistema
    - Registrado en DatabaseSeeder para ejecución automática

--- database/seeders/Sectional/SectionalSeeder.php
+++ database/seeders/Sectional/SectionalSeeder.php
@@ -10,23 +10,50 @@ class SectionalSeeder extends Seeder
     public function run(): void
     {
         $sectionals = [
-            ['name' => 'Santander'],
-            ['name' => 'San Andres'],
-            ['name' => 'Caqueta'],
+            // 26 Direcciones Seccionales oficiales
+            ['name' => 'Antioquia'],
+            ['name' => 'Atlántico'],
+            ['name' => 'Bogotá D.C.'],
+            ['name' => 'Bolívar'],
+            ['name' => 'Boyacá'],
+            ['name' => 'Caldas'],
+            ['name' => 'Caquetá'],
+            ['name' => 'Casanare'],
+            ['name' => 'Cauca'],
+            ['name' => 'Cesar'],
+            ['name' => 'Chocó'],
+            ['name' => 'Córdoba'],
+            ['name' => 'Cundinamarca'],
+            ['name' => 'La Guajira'],
+            ['name' => 'Huila'],
+            ['name' => 'Magdalena'],
             ['name' => 'Meta'],
+            ['name' => 'Nariño'],
+            ['name' => 'Norte de Santander'],
+            ['name' => 'Quindío'],
+            ['name' => 'Risaralda'],
+            ['name' => 'Santander'],
+            ['name' => 'Sucre'],
+            ['name' => 'Tolima'],
+            ['name' => 'Valle del Cauca'],
+
+            // 4 Oficinas regionales
+            ['name' => 'Amazonas'],
+            ['name' => 'Arauca'],
+            ['name' => 'Putumayo'],
+            ['name' => 'San Andrés, Providencia y Santa Catalina'],
         ];
 
         foreach ($sectionals as $sectionalData) {
             $sectional = Sectional::create($sectionalData);
 
-            // Crear auditoría simulando que lo hizo el sistema
             $sectional->audits()->create([
-                'user_name'      => "Sistema",
-                'rol_name'       => "Sistema",
+                'user_name'      => 'Sistema',
+                'rol_name'       => 'Sistema',
                 'date_time'      => now(),
                 'action_execute' => 'Creado',
                 'status_old'     => null,
-                'status_new'     => "Activo",
+                'status_new'     => 'Activo',
             ]);
         }
     }
